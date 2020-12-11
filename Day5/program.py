33140dbc5479a1d241247fd144ead94a2b6cf912
CAG2Mark <markng989@gmail.com>
2020-12-11 08:19:55 +0800

    Update program.py

--- Day5/program.py
+++ Day5/program.py
@@ -7,15 +7,15 @@ while True:
         row = 0
 
         for i in range(7):
-            if ln[i] == 'B': row = row + cur
-            cur = cur / 2
+            if ln[i] == 'B': row += cur
+            cur //= 2
 
         cur = 4
         col = 0
 
         for i in range(7, 10):
-            if ln[i] == 'R': col = col + cur
-            cur = cur / 2
+            if ln[i] == 'R': col += cur
+            cur //= 2
 
         id = row * 8 + col
 
